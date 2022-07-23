e55aa87c0785da432b5bba473f74dab6ae7933f5
NightFyre <80198020+xCENTx@users.noreply.github.com>
2022-07-23 14:14:00 -0400

    Create Class
    Wrapped Functions and Variables inside of "Process" Class.

--- Helper.h
+++ Helper.h
@@ -1,38 +1,67 @@
 #pragma once
 #include <Windows.h>
 #include <iostream>
-#include "atlstr.h"
+namespace SPTarkov {
 
-///	OBTAIN PATH
-LPCWSTR QUICK_PATHS[2]{ L"Aki.Server.exe", L"Aki.Launcher.exe" };
-LPCWSTR SERVER_PATH = L"Aki.Server.exe";
-LPCWSTR LAUNCHER_PATH = L"Aki.Launcher.exe";
-LPCWSTR GAME_PATH = L"EscapeFromTarkov.exe";
-LPCWSTR SERVER_CONFIG_PATH = L"Aki.Sever.Config.exe";
+	class Process {
+	public:
 
-/// PROCESS INFORMATION
-STARTUPINFO sINFO;
-PROCESS_INFORMATION pINFO;
-DWORD dwExit;
+		/// PROCESS INFORMATION
+		STARTUPINFO sINFO;
+		PROCESS_INFORMATION pINFO;
+		DWORD dwExit;
 
-void AllocZeroMem(PROCESS_INFORMATION pINFO, STARTUPINFO sINFO)
-{
-	ZeroMemory(&sINFO, sizeof(sINFO));
-	sINFO.cb = sizeof(sINFO);
-	ZeroMemory(&pINFO, sizeof(pINFO));
-}
+		///	PATH
+		LPCWSTR PATHS[2]{ L"Aki.Server.exe", L"Aki.Launcher.exe" };
 
-void ClearZeroMem(PROCESS_INFORMATION pINFO, STARTUPINFO sINFO)
-{
-	CloseHandle(&pINFO.hProcess);
-	CloseHandle(&pINFO.hThread);
-	memset(&sINFO, NULL, sizeof(sINFO));
-	memset(&pINFO, NULL, sizeof(pINFO));
-}
+		///	PATH EXAMPLES
+		//LPCWSTR SERVER_PATH = L"Aki.Server.exe";
+		//LPCWSTR LAUNCHER_PATH = L"Aki.Launcher.exe";
+		//LPCWSTR GAME_PATH = L"EscapeFromTarkov.exe";
+		//LPCWSTR SERVER_CONFIG_PATH = L"Aki.Sever.Config.exe";		//	KMC SERVER VALUE MODIFIER 1.5.1 [https://hub.sp-tarkov.com/files/file/308-kmc-server-value-modifier/]
 
-void ProcessRunning(PROCESS_INFORMATION pINFO, STARTUPINFO sINFO, DWORD ExitCode)
-{
-	Sleep(1000);
-	if (!GetExitCodeProcess(pINFO.hProcess, &ExitCode))
-		ClearZeroMem(pINFO, sINFO);
+		void AllocZeroMem(PROCESS_INFORMATION pINFO, STARTUPINFO sINFO);
+		void ClearZeroMem(PROCESS_INFORMATION pINFO, STARTUPINFO sINFO);
+		void ProcessRunning(PROCESS_INFORMATION pINFO, STARTUPINFO sINFO, DWORD ExitCode);
+		bool Execute();
+	};
+
+	void Process::AllocZeroMem(PROCESS_INFORMATION pINFO, STARTUPINFO sINFO)
+	{
+		ZeroMemory(&sINFO, sizeof(sINFO));
+		sINFO.cb = sizeof(sINFO);
+		ZeroMemory(&pINFO, sizeof(pINFO));
+		Sleep(1000);
+	}
+
+	void Process::ClearZeroMem(PROCESS_INFORMATION pINFO, STARTUPINFO sINFO)
+	{
+		CloseHandle(&pINFO.hProcess);
+		CloseHandle(&pINFO.hThread);
+		memset(&sINFO, NULL, sizeof(sINFO));
+		memset(&pINFO, NULL, sizeof(pINFO));
+	}
+
+	void Process::ProcessRunning(PROCESS_INFORMATION pINFO, STARTUPINFO sINFO, DWORD ExitCode)
+	{
+		Sleep(2000);
+		if (!GetExitCodeProcess(pINFO.hProcess, &ExitCode))
+			ClearZeroMem(pINFO, sINFO);
+	}
+
+	bool Process::Execute()
+	{
+		int dwSize = sizeof(PATHS) / sizeof(PATHS[0]);
+		for (int i = 0; i < sizeof(PATHS); i++)
+		{
+			AllocZeroMem(pINFO, sINFO);
+			if (!CreateProcess(PATHS[i], NULL, NULL, NULL, FALSE, NULL, NULL, NULL, &sINFO, &pINFO)) {
+				ClearZeroMem(pINFO, sINFO);
+				MessageBoxA(NULL, "LAUNCH PROCEDURE FAILED\n\nEXITING . . .", "LAUNCH ERROR", MB_ICONERROR);
+				return EXIT_FAILURE;
+			}
+			ProcessRunning(pINFO, sINFO, dwExit);
+		}
+		return EXIT_SUCCESS;
+	}
 }
--- SPTarkov-Launcher.cpp
+++ SPTarkov-Launcher.cpp
@@ -2,18 +2,7 @@
 
 int main()
 {
-	int dwSize = sizeof(QUICK_PATHS) / sizeof(QUICK_PATHS[0]);
-	for (int i = 0; i < sizeof(QUICK_PATHS); i++)
-	{
-		AllocZeroMem(pINFO, sINFO);
-		if (!CreateProcess(QUICK_PATHS[i], NULL, NULL, NULL, FALSE, NULL, NULL, NULL, &sINFO, &pINFO)) {
-			ClearZeroMem(pINFO, sINFO);
-			MessageBoxA(NULL, "LAUNCH PROCEDURE FAILED\n\nEXITING . . .", "LAUNCH ERROR", MB_ICONERROR);
-			return EXIT_FAILURE;
-		}
-		ProcessRunning(pINFO, sINFO, dwExit);
-	}
-
-	///	EXIT
-	return EXIT_SUCCESS;
+	using namespace SPTarkov;
+	Process Proc{};
+	return Proc.Execute();
 }
